7f631171bee6e5d396d28609df4dbbae67aa2cb8
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2024-12-25 02:22:02 +0900

    Update DigitalNetGeneralBase.py

--- qmcutility/DigitalNetGeneralBase.py
+++ qmcutility/DigitalNetGeneralBase.py
@@ -1,38 +1,50 @@
 class DigitalNetGeneralBase:
     def __init__(self, matrices, base):
+        """
+        self.matrices: list of matrices
+        self.base: base of the lattice
+        self.m: precision
+        self.n: log_b(number_of_points)
+        """
         self.matrices = matrices[::]
-        self.dim = len(matrices)
-        self.m, self.n = matrices[0].shape # m: precision, n: log_b(number_of_points)
+        self.m, self.n = matrices[0].shape
         for M in matrices:
             assert M.shape == (self.m, self.n)
         self.base = base
-    
+        self.coeff = np.power(1/self.base, np.arange(1,self.m+1))
+
     def generate_all_points(self):
         """
         Generate all points in the unit cube
         """
-        ratio = np.power(1/self.base, np.arange(1,self.m+1))
-
         points = np.zeros((self.base**self.n,len(self.matrices)))
         for i in range(self.base**self.n):
             vec = self._int_to_vec(i, self.n, self.base)
             for j,Mj in enumerate(self.matrices):
-                points[i,j] = (ratio@(Mj@vec%self.base)).item()
+                points[i,j] = (self.coeff@(Mj@vec%self.base)).item()
         return points
 
     def generate_all_digitally_shifted_points(self,shifts):
         """
         Generate all digitally shifted points in the unit cube
         """
-        ratio = np.power(1/self.base, np.arange(1,self.m+1))
-
         points = np.zeros((self.base**self.n,len(self.matrices)))
 
         for i in range(self.base ** self.n):
             vec = self._int_to_vec(i, self.n, self.base)
             for j,Mj in enumerate(self.matrices):
-                points[i,j] = (ratio@((Mj@vec+shifts[j])%self.base)).item()
+                points[i,j] = (self.coeff@((Mj@vec+shifts[j])%self.base)).item()
         return points
+
+    def kth_vectors(self, k: int):
+        vec = self._int_to_vec(k, self.n, self.base)
+        return [(Mj@vec%self.base) for j,Mj in enumerate(self.matrices)]
+
+    def kth_vectors_transposed(self, k: int): # transposed for visibility
+        return [M.T for M in self.kth_vectors(k)]
+
+    def kth_points(self, k: int):
+        return np.array([self.coeff@vec for vec in self.kth_vectors(k)])
 
     @staticmethod
     def polynomial_lattice(denom, numers, base, n=None, precision=None):
@@ -55,7 +67,7 @@ class DigitalNetGeneralBase:
 
         """
         if q_i(x)/p(x) = (poly) + u_1 x^{-1} + u_2 x^{-2} + ... + u_{m+n-1}x^{m+n-1},
-        then the generating matrix C is Hankel with C[i][j] = u_{i+j},  
+        then the generating matrix C is Hankel with C[i][j] = u_{i+j},
         [[u_1 u_2 u_3 ...]
          [u_2 u_3 u_4 ...]
           ...
@@ -72,9 +84,9 @@ class DigitalNetGeneralBase:
             for i in range(precision):
                 matrix[i,:] = result[i:i+n]
             matrices.append(matrix)
-        
+
         return DigitalNetGeneralBase(matrices,base)
-    
+
     @staticmethod
     def _int_to_vec(N: int, m: int, b: int) -> np.ndarray:
         """
@@ -84,4 +96,3 @@ class DigitalNetGeneralBase:
         for i in range(m):
             N,res[i] = divmod(N,b)
         return np.array(res).reshape(-1, 1)
-
