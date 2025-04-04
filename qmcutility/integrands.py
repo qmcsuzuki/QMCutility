e1a5b8fccafcf24e662498f810d027de9728b1df
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-04-04 20:58:32 +0900

    Update integrands.py

--- qmcutility/integrands.py
+++ qmcutility/integrands.py
@@ -14,8 +14,12 @@ def generate_normalized_expsum(weights):
       weights: d-dim vector, weights for each dimension
     """
     C = np.prod((np.exp(weights)-1)/weights) # normalizing constant
-    f = lambda points: np.exp(np.sum(points*weights,1))/C
+    f = lambda points: np.exp(np.sum(points*weights, axis=1))/C
     return (f, 1)
+
+def generate_sinsum(weights):
+    f = lambda points: np.sin(np.sum(2*np.pi*points, axis=1))
+    return (f, 0)
 
 """
 f(x) = \prod(1 + w_j * B_2(x_j))
@@ -24,7 +28,7 @@ true_value = 1
 def generate_Bernoulli2(weights):
     def f(points):
         B2 = (points - 1)*points + 1/6
-        return np.prod(1 + weights*B2,1)
+        return np.prod(1 + weights*B2, 1)
     return (f, 1)
 
 """
@@ -32,11 +36,10 @@ f(x) = \prod(1 + w_j * B_3(x_j))
 true_value = 1
 """
 def generate_Bernoulli3(weights):
-    return np.prod(1 + weights*B3,1)
+    return np.prod(1 + weights*B3, 1)
     def f(points):
         B3 = ((points - 3/2)*points + 1/2)*points
-        return np.prod(1 + weights*B3,1)
+        return np.prod(1 + weights*B3, 1)
     return (f, 1)
-
 
 
