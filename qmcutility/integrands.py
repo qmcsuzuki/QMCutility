6d1e579cb986020989a0fee65b0347aeb212f493
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2025-04-04 20:29:08 +0900

    Create integrands.py

---
+++ qmcutility/integrands.py
@@ -0,0 +1,42 @@
+"""
+every function returns the pair (integrand, true_value)
+- integrand: function from points (n*d ndarray) to values (n*1 vector)
+- true_value: integration value
+"""
+
+"""
+f(x) = exp(sum(w1*x1+...+ws*xs))/C where C is normalizing constant
+true_value = 1
+"""
+def generate_normalized_expsum(weights):
+    """
+    Args:
+      weights: d-dim vector, weights for each dimension
+    """
+    C = np.prod((np.exp(weights)-1)/weights) # normalizing constant
+    f = lambda points: np.exp(np.sum(points*weights,1))/C
+    return (f, 1)
+
+"""
+f(x) = \prod(1 + w_j * B_2(x_j))
+true_value = 1
+"""
+def generate_Bernoulli2(weights):
+    def f(points):
+        B2 = (points - 1)*points + 1/6
+        return np.prod(1 + weights*B2,1)
+    return (f, 1)
+
+"""
+f(x) = \prod(1 + w_j * B_3(x_j))
+true_value = 1
+"""
+def generate_Bernoulli3(weights):
+    return np.prod(1 + weights*B3,1)
+    def f(points):
+        B3 = ((points - 3/2)*points + 1/2)*points
+        return np.prod(1 + weights*B3,1)
+    return (f, 1)
+
+
+
